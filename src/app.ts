6162c469fe31935fde79994d0bbe1e9485008b01
Michał <ursusen@gmail.com>
2020-11-06 21:08:42 +0100

    update token

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ class App {
     public initializeMiddlewares() {
         this.app.use(bodyParser.urlencoded({extended:false}));
         this.app.use(bodyParser.json());
-        this.app.use(cors());
+        this.app.use(cors({credentials:true}));
         this.app.use(cookieParser());
     }
 
