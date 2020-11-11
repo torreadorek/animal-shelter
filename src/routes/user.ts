64a2d41d0b52d3590d0f9573ce157cb7c8798547
Michał <ursusen@gmail.com>
2020-11-11 17:44:57 +0100

    update walks

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -10,8 +10,9 @@ class User {
 
     constructor(){
         this.router.post('/donation/new',this.newDonate);
-        this.router.patch('/walk/new',this.newWalk);
+        this.router.post('/walk/new',this.newWalk);
         this.router.put('/walk/overview',this.getWalks);
+        this.router.post('/help/new',this.newHelp);
     }
 
      newDonate =  async (req:Request,res:Response) => {
@@ -44,6 +45,54 @@ class User {
      }
 
      newWalk = async (req:Request,res:Response) => {
+      try{
+        const {steps} = req.body;
+        const decodedToken:any = checkToken(req.body.token,req.cookies.token);
+        const newHelp = await UserModel.updateOne({
+            authId:decodedToken.authId
+        },{
+            $push:{
+                walks:{
+                    steps:steps
+                }
+            }
+        })
+        if(newHelp) res.status(200).json('success');
+        else res.status(403).json('failure');
+      }catch(error){
+            console.log('error:',error)
+            res.status(500).json('Something went wrong');
+      }
+    }
+
+     getWalks = async (req:Request,res:Response) => {
+        try{ 
+            const decodedToken:any = checkToken(req.body.token,req.cookies.token)
+                const date = new Date(Date.now())
+                console.log('date',date)
+               const walks = await UserModel.aggregate([
+                   {
+                       $filter:{
+                           input:"walks",
+                           as:"walk",
+                           cond:{
+                               $gt:new Date()
+                           }
+                       }  
+                   }
+            ])
+                if(walks) {
+                    console.log('user',walks)
+                    res.status(200).json({message:'success',walks:walks})
+                } else res.status(403).json('failure')
+            
+        }catch(error) {
+            console.log('error:',error)
+            res.status(500).json('Something went wrong');
+        }
+    }
+
+    newHelp = async (req:Request,res:Response)=>{
         try{ 
             const {startTime,endTime} = req.body
             const decodedToken:any = checkToken(req.body.token,req.cookies.token)
@@ -67,25 +116,8 @@ class User {
             console.log('error:',error)
             res.status(500).json('Something went wrong');
         }
-    }
 
-     getWalks = async (req:Request,res:Response) => {
-        try{ 
-            const decodedToken:any = checkToken(req.body.token,req.cookies.token)
-                const date = new Date()
-                console.log('date',date)
-               const walks = await UserModel.findOne({
-                    authId:decodedToken.authId
-               }).select('walks')
-                if(walks) {
-                    console.log('user',walks)
-                    res.status(200).json({message:'success',walks:walks})
-                } else res.status(403).json('failure')
-            
-        }catch(error) {
-            console.log('error:',error)
-            res.status(500).json('Something went wrong');
-        }
+      
     }
 
 }
