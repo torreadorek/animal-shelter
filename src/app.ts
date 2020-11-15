908d974c8fb83dede10b1d2cc38e8cfc96861da3
Michał <ursusen@gmail.com>
2020-11-15 19:42:54 +0100

    update origin

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ class App {
     public initializeMiddlewares() {
         this.app.use(bodyParser.urlencoded({extended:false}));
         this.app.use(bodyParser.json());
-        this.app.use(cors({credentials:true,origin:['www.projekt.mateuszdobosz.site']}));
+        this.app.use(cors({credentials:true,origin:'http://mateuszdobosz.site'}));
         this.app.use(cookieParser());
     }
 
